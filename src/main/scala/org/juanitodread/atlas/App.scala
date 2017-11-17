8fd06a14283fcaa30a476c2bcb20a84df91d9e6d
Juan Sandoval <juanitodread@gmail.com>
2017-11-16 21:58:30 -0600

    Add akka-http and a dummy test

--- src/main/scala/org/juanitodread/atlas/App.scala
+++ src/main/scala/org/juanitodread/atlas/App.scala
@@ -1,7 +1,27 @@
 package org.juanitodread.atlas
 
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import scala.io.StdIn
+
 object App {
   def main(args: Array[String]): Unit = {
-    println("Hello world from Docker :D")
+    println("Starting server...")
+
+    implicit val system = ActorSystem("atlas")
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
+
+    val route = path("hello") {
+      get {
+        complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      }
+    }
+
+    Http().bindAndHandle(route, "0.0.0.0", 8080)
+    println("Server started at 0.0.0.0")
   }
 }
