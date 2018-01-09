88a426366dea3d772bb1180211dbe7f7b60e81ba
juanitodread <juanitodread@gmail.com>
2018-01-09 17:22:42 -0600

    Fix #3

--- src/main/scala/org/juanitodread/atlas/App.scala
+++ src/main/scala/org/juanitodread/atlas/App.scala
@@ -1,10 +1,12 @@
 package org.juanitodread.atlas
 
+import org.juanitodread.atlas.conf.AppConf
 import com.typesafe.scalalogging.LazyLogging
 
-object App extends LazyLogging {
+object App extends AppConf with LazyLogging {
   def main(args: Array[String]): Unit = {
     logger.debug("Init")
     logger.info("Inint")
+    logger.info(simpleConf)
   }
 }
